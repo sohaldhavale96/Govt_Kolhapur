96a5aa260423d5bea891863189122e96f639f291
sohaldhavale96 <sohaldhavale96@gmail.com>
2024-09-05 17:21:49 +0530

    About section Done .

--- js/about.js
+++ js/about.js
@@ -71,5 +71,65 @@ function horizontalAnimation(){
 
 }
 
+function accordins() {
+    const items = document.querySelectorAll(".item");
+    const imageurl = [
+        "#4286F5",
+        "#EA4235",
+        "#EB8600",
+        "#1E8D3E",
+        "#F9AB01"
+    ]
+
+    items.forEach((item,index) => {
+        const itemsHead = item.querySelector(".feature-title h1");
+        const itemsHeadi = item.querySelector(".feature-title i");
+        const desc = item.querySelector(".feature-description p");
+        desc.style.display = "none";
+        let isOpen = false;
+
+
+        item.addEventListener("click", () => {
+            isOpen = !isOpen;
+
+            if (isOpen) {
+                item.style.height = "50vh";
+                item.style.backgroundColor = `${imageurl[index]}`
+                item.backgroundPosition = "center";
+                item.backgroundSize = "cover";
+                item.backgroundRepeat = "no-repeat";
+                itemsHead.style.color = "#000000";
+                itemsHeadi.style.color = "#000000";
+                desc.style.display = "block";
+                desc.style.color = "#000";
+            } else {
+                item.style.height = "15vh";
+                item.style.background = "#ffffff4a";
+                itemsHead.style.color = "#ffffff";
+                itemsHeadi.style.color = "#ffffff";
+                desc.style.display = "none";
+                desc.style.color = "transparent";
+            }
+
+            // Close all other items
+            items.forEach((otherItem) => {
+                if (otherItem !== item) {
+                    otherItem.style.height = "15vh";
+                    otherItem.style.background = "#ffffff4a";
+                    const otherHead = otherItem.querySelector(".feature-title h1");
+                    const otherHeadi = otherItem.querySelector(".feature-title i");
+                    const otherDesc = otherItem.querySelector(".feature-description p");
+                    otherHead.style.color = "#ffffff";
+                    otherHeadi.style.color = "#ffffff";
+                    otherDesc.style.display = "none";
+                }
+            });
+        });
+    });
+}
+
+
+
 firstAnimation()
 horizontalAnimation()
+accordins()
